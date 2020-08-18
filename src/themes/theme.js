24cddf0b2941d63fc11883f14e133be2517ab811
Vivek Mittal <42748450+vivek1909@users.noreply.github.com>
2020-08-19 01:12:47 +0530

    styled components

---
+++ src/themes/theme.js
@@ -0,0 +1,35 @@
+export default {
+  fonts: {
+    main: "Montserrat, sans-serif",
+    code: "Poppins, monospace",
+  },
+  colors: {
+    main1: "hsl(207,70%,59%)",
+    main2: "hsl(207,70%,94%)",
+    dark1: "hsl(227,2%,12%)",
+    dark2: "hsl(227,2%,26%)",
+    dark3: "hsl(227,2%,64%)",
+    light1: "hsl(0,0%,97%)",
+    light2: "hsl(0,0%,99%)",
+  },
+  breakpoints: {
+    mobile: "only screen and (max-width: 50rem)",
+    tablet: "only screen and (max-width: 65rem)",
+  },
+  spacings: {
+    xxSmall: ".25rem",
+    xSmall: ".5rem",
+    small: "1rem",
+    medium: "2rem",
+    large: "3rem",
+    xLarge: "4rem",
+    xxLarge: "6rem",
+  },
+  animations: {
+    button: "box-shadow 0.3s ease",
+    link: "color 0.2s ease",
+  },
+  shadows: {
+    shadow1: "0px 5px 20px rgba(30,30,31,0.05)",
+  },
+}
