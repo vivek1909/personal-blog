de33a427eb40f377124db962c646c2956e26998a
Vivek Mittal <42748450+vivek1909@users.noreply.github.com>
2020-08-19 15:23:44 +0530

    programatically generate pages

---
+++ gatsby-node.js
@@ -0,0 +1,45 @@
+exports.createPages = async function ({ actions, graphql }) {
+  const { data } = await graphql`
+    query {
+      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+        edges {
+          node {
+            frontmatter {
+              slug
+            }
+            id
+          }
+        }
+      }
+    }
+  `
+
+  // create paginated pages for posts
+  const postPerPage = 3
+  const totalPages = Math.ceil(data.allMdx.edges.length / postPerPage)
+
+  Array.from({ length: totalPages }).forEach((_, i) => {
+    actions.createPages({
+      path: i === 0 ? `/` : `/{i + 1}`,
+      component: require.resolve("./src/templates/allPosts.js"),
+      context: {
+        limit: postPerPage,
+        skip: i * postPerPage,
+        totalPages,
+        currentPage: i + 1,
+      },
+    })
+  })
+
+  // create single blog posts
+  data.allMdx.edges.forEach(edge => {
+    const slug = edge.node.frontmatter.slug
+    const id = edge.node.id
+
+    actions.createPages({
+      path: slug,
+      component: require.resolve("./src/templates/singlePost.js"),
+      context: { id },
+    })
+  })
+}
